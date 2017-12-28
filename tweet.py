d424222406c1d804ba53374f3bbd921e6aeaacc6
BananaKetchup <31089758+charlenenicer@users.noreply.github.com>
2017-12-27 20:15:58 -0800

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -2,11 +2,11 @@ import tweepy
 import csv
 from textblob import TextBlob
 
-consumer_key = 'nGkTtnuVewCUNfGLAPbEplr7U'
-consumer_secret = 'Cc5HCGXX2Ph9mezTJammibRKRLb83kmCMQIi6ElFzJPcKy4bvx'
+consumer_key = ''
+consumer_secret = ''
 
-access_token = '319715947-3YBGMUPTpZnSViTV9PfKocwcHnKGnV6JZZ9Z9Hhg'
-access_token_secret = 'zuZjFpMDWJ5KG6yrpDXJ5TDuyB362KKhvaZhFsPrI30Aj'
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) #authentication
 auth.set_access_token(access_token, access_token_secret)
