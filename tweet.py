053399f779d3f6ee95c92bbe524c1d4c8e171ee4
BananaKetchup <31089758+charlenenicer@users.noreply.github.com>
2018-07-17 17:10:27 -0700

    added some comments

--- tweet.py
+++ tweet.py
@@ -1,20 +1,28 @@
+#import library to give access to twitter
 import tweepy
+# import library to give a access to CSV
 import csv
+# import TextBlob library
 from textblob import TextBlob
 
+#details ... details..
 consumer_key = ''
 consumer_secret = ''
 
 access_token = ''
 access_token_secret = ''
 
+#Create authorization
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) #authentication
 auth.set_access_token(access_token, access_token_secret)
 
+#Use Twitter API
 api = tweepy.API(auth) 
 
+#gather tweets
 public_tweets = api.search('UST Paskuhan')
 
+#write on csv
 download_dir = "example.csv"
 csv = open(download_dir, "w")
 columnTitleRow = "tweet, analysis\n"
